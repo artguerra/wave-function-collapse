468f89934aecd7f96fb177ec6274f068c6934684
Arthur Guerra <conta.arthurguerra@gmail.com>
2026-04-14 15:19:13 +0200

    fix base url for deployed website

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 
 export default {
   resolve: {
+    base: "wave-function-collapse",
     alias: {
       "@": path.resolve(__dirname, "./src/"),
       "@assets": path.resolve(__dirname, "./assets/"),
